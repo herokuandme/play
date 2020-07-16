7fcc27d8f1deaae1b5ec7903bb5875ef4ba106e8
VishalR8 <54264240+VishalR8@users.noreply.github.com>
2020-07-16 20:33:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+console.log('Testing')
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
